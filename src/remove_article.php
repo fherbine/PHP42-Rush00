7c57c6af8177d96735c52b616e985b2a342467f9
Paul PRIKAZSKY <paul.prikazsky@gmail.com>
2018-04-01 18:37:45 +0200

    Ajout d'un cookie de panier

--- src/remove_article.php
+++ src/remove_article.php
@@ -6,12 +6,10 @@ include_once ("modele/article_helper.php");
 
 function remove_article()
 {
-  $bdd_file_path = ROOT . DS . 'bdd' . DS . 'article';
-
-  $articles = get_articles(BDD_PATH, $bdd_file_path);
+  $articles = get_articles(BDD_PATH, BDD_ARTICLE);
   if (article_exits($articles, @$_GET['art_title']) !== FALSE)
   {
-      delete_article($articles, @$_GET['art_title'], $bdd_file_path);
+      delete_article($articles, @$_GET['art_title'], BDD_ARTICLE);
       redirect('../views/account.phtml', 302);
   }
   else redirect('../views/account.phtml', 302);
