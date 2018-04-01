be4d4bc09094e18e2f8f81e64b606310d7dbc957
Paul PRIKAZSKY <paul.prikazsky@gmail.com>
2018-04-01 23:41:44 +0200

    Ajout

--- install.php
+++ install.php
@@ -11,7 +11,7 @@ include_once ('src/modele/article_helper.php');
 get_articles(BDD_PATH, BDD_ARTICLE);
 get_categories(BDD_PATH, BDD_CATEGORIE);
 get_orders(BDD_PATH, BDD_ORDER);
-get_accounts(BDD_PATH, BDD_PASSWD);
+$accounts = get_accounts(BDD_PATH, BDD_PASSWD);
 
 $values['login'] = 'admin';
 $values['realname'] = 'admin';
