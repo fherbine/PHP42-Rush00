34b22369077dd9c50b7a3a127e29793261830241
Paul PRIKAZSKY <paul.prikazsky@gmail.com>
2018-04-01 20:03:28 +0200

    Ajout de la suppression des éléments dans le panier

--- src/modele/cookie_helper.php
+++ src/modele/cookie_helper.php
@@ -28,6 +28,11 @@ function add_user_in_cookie($cookie_value, $login)
   return serialize($cookie_value);
 }
 
+function set_cookie_content($cookie_content)
+{
+    return serialize($cookie_content);
+}
+
 function get_cookie_content($cookie_content)
 {
     return unserialize($cookie_content);
---
+++ src/remove_to_cart.php
@@ -0,0 +1,28 @@
+<?php
+
+include ('const.php');
+include ('modele/render.php');
+include ('modele/article_helper.php');
+include ('modele/cookie_helper.php');
+
+function remove_to_cart()
+{
+    $cooki_name;
+
+    $cookie_name = 'card';
+    if (get_article_by_name(BDD_PATH, BDD_ARTICLE, @$_GET['art_title']) !== FALSE)
+    {
+      $cookie_content = get_cookie_content($_COOKIE[$cookie_name]);
+      if (isset($cookie_content['item']))
+      {
+          if (array_key_exists(@$_GET['art_title'], $cookie_content['item']))
+            unset($cookie_content['item'][@$_GET['art_title']]);
+          $cookie_content= set_cookie_content($cookie_content);
+          create_cookie($cookie_name, $cookie_content);
+      }
+    }
+    redirect("../views/cart.phtml", 302);
+}
+
+remove_to_cart();
+?>
