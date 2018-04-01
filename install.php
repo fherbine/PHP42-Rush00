28b45a9c66c2159fdca48c9cae37a2351949654c
Paul PRIKAZSKY <paul.prikazsky@gmail.com>
2018-04-01 23:10:28 +0200

    Ajout du fichier install

---
+++ install.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once ('src/const.php');
+include_once ('src/modele/user_helper.php');
+include_once ('src/modele/user_helper.php');
+include_once ('src/modele/categroie_helper.php');
+include_once ('src/modele/order_helper.php');
+include_once ('src/modele/article_helper.php');
+
+get_articles(BDD_PATH, BDD_ARTICLE);
+get_categories(BDD_PATH, BDD_CATEGORIE);
+get_orders(BDD_PATH, BDD_ORDER);
+get_users(BDD_PATH, BDD_PASSWD);
+
+$values['login'] = 'admin';
+$values['realname'] = 'admin';
+$values['passwd'] = 'admin';
+$values['admin'] = TRUE;
+
+create_account($accounts, $values['login'], $values, BDD_PASSWD);
+?>
